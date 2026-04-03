9e9d737f96edb3d4afa045ec2da509c37c8b6cea
Henry Crosswell <henry.crosswell100@gmail.com>
2026-04-03 17:33:29 +0100

    test: added two_sum function to test PR

--- src/practice/main.py
+++ src/practice/main.py
@@ -2,5 +2,17 @@ def main():
     print("Hello from practice!")
 
 
+def two_sum():
+    nums = [2, 7, 11, 15]
+    target = 9
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            print([num_map[complement], i])
+        num_map[num] = i
+
+
 if __name__ == "__main__":
     main()
+    two_sum()
