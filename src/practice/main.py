a27fb371f1f01a15ba5ca7ce4875e2a46ec96b45
Henry Crosswell <52281444+HenryCrosswell@users.noreply.github.com>
2026-04-03 22:20:04 +0100

    Feat/implement ci cd (#2)
    * test: added two_sum function to test PR
    
    * implement auto-versioning for build
    
    * Add CICD workflow yaml
    
    * bug: remove double project call in toml
    
    * chore: update sync to include dev
    
    * test: test pre-commit works
    
    * bug: update toml to remove ruff prompt
    
    * chore: add CI badge to README
    
    * chore: add pytest-cov to dev package
    
    * chore: add a test for main
    
    * test: include tests in main file
    
    ---------
    
    Co-authored-by: Henry Crosswell <henry.crosswell100@gmail.com>
    
    Solves issue #3

--- src/practice/main.py
+++ src/practice/main.py
@@ -2,5 +2,16 @@ def main():
     print("Hello from practice!")
 
 
+def two_sum(nums: list[int], target: int) -> list[int]:
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
+
+
 if __name__ == "__main__":
     main()
+    print(two_sum([2, 7, 11, 15], 9))
